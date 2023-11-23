de4aae3c56f6f04bcbe147841b2288d39d07c05a
David Sims <david.sims098@gmail.com>
2023-11-23 15:57:46 -0500

    engine changes in package

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,6 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Landing' });
 });
 
-/* GET Database Page page. */
-// router.get('/tournaments', function(req, res, next) {
-//   res.render('tournaments', { title: 'Tournaments' });
-// });
-// router.get('/tournaments', async (req, res) => {
-//   console.log('tourney.js');
-//   try {
-//       const items = await Tourney.find();
-//       console.log(items);
-//       res.render('tourney.ejs', { items, title: "Tournaments"}); // Render the EJS view and pass items as a local variable
-//   } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
 
 module.exports = router;
