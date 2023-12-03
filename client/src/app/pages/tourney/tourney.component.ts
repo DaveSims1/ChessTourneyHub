f53712f1deb78d64e72fdc2b1e0d3913635e58d2
IniyanThil <111782904+IniyanThileep@users.noreply.github.com>
2023-12-03 07:55:20 -0500

    Finished design for backend, added angualr partials

---
+++ client/src/app/pages/tourney/tourney.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tourney',
+  standalone: true,
+  imports: [],
+  templateUrl: './tourney.component.html',
+  styleUrl: './tourney.component.css'
+})
+export class TourneyComponent {
+
+}
---
+++ client/src/app/pages/tourneyedit/tourneyedit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tourneyedit',
+  standalone: true,
+  imports: [],
+  templateUrl: './tourneyedit.component.html',
+  styleUrl: './tourneyedit.component.css'
+})
+export class TourneyeditComponent {
+
+}
